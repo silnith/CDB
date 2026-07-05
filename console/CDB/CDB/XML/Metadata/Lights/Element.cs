b1145322517b20d54add4e7f64ce7600195a3b9d
silnith <silnith@gmail.com>
2026-07-04 19:55:26 -0500

    Add some classes for deserializing Metadata/Lights.xml

---
+++ console/CDB/CDB/XML/Metadata/Lights/Element.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Silnith.CDB.XML.Metadata.Lights;
+
+[XmlRoot("Lights", Namespace = "http://www.opengis.net/cdb/1.2/Lights")]
+public class Element
+{
+    [XmlElement("Light")]
+    public List<Light.Element> Lights
+    {
+        get;
+        set;
+    }
+
+    /// <summary>
+    /// This attribute represents the version number of this file. It has two components: major.minor.
+    /// </summary>
+    [XmlAttribute("version")]
+    [RegularExpression("([1-9]([0-9])*[.]([0-9])+)")]
+    public string Version
+    {
+        get;
+        set;
+    }
+}
---
+++ console/CDB/CDB/XML/Metadata/Lights/Light/Element.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Silnith.CDB.XML.Metadata.Lights.Light;
+
+/// <summary>
+/// A light has a type and a code.
+/// A light can optionally have a description and a list of child lights.
+/// </summary>
+[XmlType("Light", TypeName = "Light", Namespace = "http://www.opengis.net/cdb/1.2/Lights")]
+public class Element
+{
+    [XmlElement("Description")]
+    public string? Description
+    {
+        get;
+        set;
+    }
+
+    [XmlElement("Light")]
+    public List<Element> ChildLights
+    {
+        get;
+        set;
+    }
+
+    [XmlAttribute("type")]
+    public string Type
+    {
+        get;
+        set;
+    }
+
+    [XmlAttribute("code")]
+    [Range(0, 9999)]
+    public int Code
+    {
+        get;
+        set;
+    }
+}
