2b1a90b4f84b5dd915ca4f92de19d200dada8699
silnith <silnith@gmail.com>
2026-07-04 19:54:25 -0500

    Capture some timing data instead of spewing lots of output.

--- console/CDB/CDB.Importer/Program.cs
+++ console/CDB/CDB.Importer/Program.cs
@@ -58,7 +58,13 @@ internal class Program
         string cdbName = "CDB";
         DirectoryInfo cdbRoot = new(cdbName);
 
+        DateTimeOffset start = DateTimeOffset.UtcNow;
+
         sqliteDataStore.ImportDirectory(cdbName, cdbRoot, host.Services);
+
+        DateTimeOffset end = DateTimeOffset.UtcNow;
+
+        Console.WriteLine("Import time: {0}", end - start);
 
         using ICDB cdb = new SQLCDB(cdbName, cdbRoot, sqliteDataStore);
         CDBInformation cdbInformation = new();
@@ -66,26 +72,32 @@ internal class Program
 
         foreach ((int code, string name) in cdbInformation.DatasetNames)
         {
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "{0:D3}_{1}", code, name));
+            //Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "{0:D3}_{1}", code, name));
         }
         foreach (FeatureCode featureCode in cdbInformation.ValidFeatureSubcodes.Keys)
         {
-            Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "{0} = {1}, {2}, {3}",
-                featureCode.Code,
-                cdbInformation.FeatureCategoryNames[featureCode.Category],
-                cdbInformation.FeatureSubcategoryNames[featureCode.Category + featureCode.Subcategory],
-                cdbInformation.FeatureTypeNames[featureCode]));
+            //Console.WriteLine(string.Format(CultureInfo.InvariantCulture, "{0} = {1}, {2}, {3}",
+            //    featureCode.Code,
+            //    cdbInformation.FeatureCategoryNames[featureCode.Category],
+            //    cdbInformation.FeatureSubcategoryNames[featureCode.Category + featureCode.Subcategory],
+            //    cdbInformation.FeatureTypeNames[featureCode]));
         }
 
+        start = DateTimeOffset.UtcNow;
+
+        int fileCount = 0;
         sqliteDataStore.WalkDirectory(cdbRoot, host.Services, (obj, fileInfo) =>
         {
             string relativePath = Path.GetRelativePath(cdb.CdbRoot.FullName, fileInfo.FullName);
             cdb.TryReadFile(relativePath, stream =>
             {
-                Console.WriteLine("Found {0}", relativePath);
+                fileCount++;
+                //Console.WriteLine("Found {0}", relativePath);
             });
         });
 
-        Console.WriteLine("Done");
+        end = DateTimeOffset.UtcNow;
+
+        Console.WriteLine("Walk time: {0}", end - start);
     }
 }
